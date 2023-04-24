97ebcb25cec6e69e5c5baef35b691b53be808eb7
Valentina <v.docool@ya.ru>
2023-04-24 16:19:33 +0300

    All commit

--- home_work/test_2_hw.py
+++ home_work/test_2_hw.py
@@ -13,11 +13,5 @@ def task_1(a, b, c, d, e):
 print(a, ' = ', type(a), ',', b, ' = ', type(b), ',', c, ' = ', type(c),  ',', d , ' = ', type(d), ',', e, ' = ', type(e))
 
 
-# Задача 2
-def task_2(a: list):
-    a = [1, 2, 3, 5, 8, 13, 21]
-    return a
 
-
-print(a)
 
